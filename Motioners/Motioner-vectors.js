72c28adbf1ac9380deb19196713aa096b5f8b586
GreatBigBore <GreatBigBore@gmail.com>
2016-09-10 14:03:01 -0700

    Forgot to pop this before merging the unit test branch. Looks ok, hope I haven't lost anything

--- Motioners/Motioner-vectors.js
+++ Motioners/Motioner-vectors.js
@@ -126,7 +126,7 @@ Rob.Motioner.prototype.getSenseVector = function(sense) {
       console.log('wtf m < c');
       this.vectors[sense].set(0, 0);
     } else {
-      this.vectors[sense].scalarMultiply(c / m);
+      this.vectors[sense].scalarMultiply(Math.abs(c / m));
       //Rob.debugText += "sv2 (" + this.vectors.smell.x + ", " + this.vectors.smell.y + ")\n"
     }
 
